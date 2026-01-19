22356611e3164106f3e03cee5d0f371d37eda33b
ccc <ccc@topdanmark.dk>
2026-01-19 15:39:14 +0800

    Fix typo in GOOGLE_CLIENT_SECRET variable and add Vercel configuration file

--- src/app/api/auth/[...nextauth]/auth-options.ts
+++ src/app/api/auth/[...nextauth]/auth-options.ts
@@ -2,13 +2,13 @@ import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
-const GOOLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOLE_CLIENT_SECRET,
+      clientSecret: GOOGLE_CLIENT_SECRET,
     }),
   ],
   pages: {
